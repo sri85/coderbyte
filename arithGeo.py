82ba0b1bf31f0cff6ed4830287f7f99216a009ba
sri85 <paisripathi@gmail.com>
2013-08-09 17:23:39 +0530

    Solution for arithGeo problem

---
+++ arithGeo.py
@@ -0,0 +1,18 @@
+def ArithGeo(lst):
+    array1 =[]
+    array2 =[]
+    for i in range(4):
+        array1.append(lst[i+1]-lst[i])
+        array2.append(lst[i+1]/lst[i])
+        
+        
+    def  checkEqual3(lst,lst1):
+        if(lst[1:] == lst[:-1]):
+            return "Arithmetic"
+
+        if (lst1[1:] == lst1[:-1]):
+            return "Geometric"
+        else:
+            return "-1"
+    return checkEqual3(array1,array2)
+print ArithGeo([1,2,3,4,6])
